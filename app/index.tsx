a25599abd6930bcbcfbb4a2690327495c2c06776
Njoroge Charles <njorodev1319@gmail.com>
2024-12-03 14:08:41 +0300

    First commit of creating the store mobile app

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text className="text-red-500">Welcome home screen</Text>
+    </View>
+  );
+};
+
+export default Home;
